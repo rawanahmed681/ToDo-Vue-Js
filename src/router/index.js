9deda051335c0486a09d1e10423d0832235b711f
MOSTAFA <rawanahmed681@gmail.com>
2024-10-16 15:46:04 +0300

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import ModalView from '../views/ModalView.vue'
 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/Modal',
-      name: 'ModalView',
-      component: ModalView
+      
     }
   ]
 })
